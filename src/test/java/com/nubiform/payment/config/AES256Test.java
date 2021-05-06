cf832b076a922c12d61ed9f84fba1b55a9fdbff9
NuBiFoRM <nubiform@me.com>
2021-05-07 00:47:41 +0900

    aes256 test

--- src/test/java/com/nubiform/payment/config/AES256Test.java
+++ src/test/java/com/nubiform/payment/config/AES256Test.java
@@ -1,5 +1,6 @@
 package com.nubiform.payment.config;
 
+import com.nubiform.payment.api.vo.Card;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,4 +15,37 @@ class AES256Test {
         assertEquals(PLAIN_TEXT, aes256.decrypt(aes256.encrypt(PLAIN_TEXT)));
     }
 
+    @Test
+    public void testCard10() throws Exception {
+        Card card = new Card();
+        card.setCard("1234567890");
+        card.setExpiration("1234");
+        card.setCvc("123");
+
+        AES256 aes256 = new AES256();
+        String encrypt = aes256.encrypt(card.toData());
+        System.out.println(encrypt);
+        Card newCard = new Card(aes256.decrypt(encrypt));
+
+        assertEquals(card.getCard(), newCard.getCard());
+        assertEquals(card.getExpiration(), newCard.getExpiration());
+        assertEquals(card.getCvc(), newCard.getCvc());
+    }
+
+    @Test
+    public void testCard16() throws Exception {
+        Card card = new Card();
+        card.setCard("1234567890123456");
+        card.setExpiration("1234");
+        card.setCvc("123");
+
+        AES256 aes256 = new AES256();
+        String encrypt = aes256.encrypt(card.toData());
+        System.out.println(encrypt);
+        Card newCard = new Card(aes256.decrypt(encrypt));
+
+        assertEquals(card.getCard(), newCard.getCard());
+        assertEquals(card.getExpiration(), newCard.getExpiration());
+        assertEquals(card.getCvc(), newCard.getCvc());
+    }
 }
