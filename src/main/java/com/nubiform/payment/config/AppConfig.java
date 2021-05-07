05edc222229f2b4232d51b2d3a2370a84dc10424
NuBiFoRM <nubiform@me.com>
2021-05-07 20:35:24 +0900

    modelMapper

--- src/main/java/com/nubiform/payment/config/AppConfig.java
+++ src/main/java/com/nubiform/payment/config/AppConfig.java
@@ -2,14 +2,25 @@ package com.nubiform.payment.config;
 
 import com.nubiform.payment.security.AES256;
 import com.nubiform.payment.security.Encryption;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.NameTokenizers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-    
+
     @Bean
     public Encryption encryption() {
         return new AES256();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setSourceNameTokenizer(NameTokenizers.UNDERSCORE)
+                .setDestinationNameTokenizer(NameTokenizers.UNDERSCORE);
+        return modelMapper;
     }
 }
