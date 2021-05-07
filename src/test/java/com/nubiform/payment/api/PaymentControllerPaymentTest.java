a9e6fc5d938405ea731bc1aa1c4dd07c6044d8a3
NuBiFoRM <nubiform@me.com>
2021-05-08 00:44:18 +0900

    payment test

---
+++ src/test/java/com/nubiform/payment/api/PaymentControllerPaymentTest.java
@@ -0,0 +1,63 @@
+package com.nubiform.payment.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nubiform.payment.repository.SentRepository;
+import com.nubiform.payment.service.PaymentService;
+import com.nubiform.payment.vo.PaymentRequest;
+import com.nubiform.payment.vo.SubmitRequest;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PaymentControllerPaymentTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    PaymentService paymentService;
+
+    @Autowired
+    SentRepository sentRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    SubmitRequest submitRequest;
+    PaymentRequest paymentRequest;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        submitRequest = new SubmitRequest();
+        submitRequest.setCard("1234567890123456");
+        submitRequest.setExpiration("1234");
+        submitRequest.setCvc("123");
+        submitRequest.setInstallment(0);
+        submitRequest.setAmount(1000L);
+
+        Long id = paymentService.submit(submitRequest).getId();
+
+        paymentRequest = new PaymentRequest();
+        paymentRequest.setId(StringUtils.leftPad(String.valueOf(id), 20, "0"));
+    }
+
+    @Test
+    public void getPayment() throws Exception {
+        mockMvc.perform(get("/api/v1/payment")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(paymentRequest)))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
