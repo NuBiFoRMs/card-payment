d8e38facbb363c581397a75b3cccaa864a2343e4
NuBiFoRM <nubiform@me.com>
2021-05-09 20:46:37 +0900

    h2

--- src/main/java/com/nubiform/payment/config/AppConfig.java
+++ src/main/java/com/nubiform/payment/config/AppConfig.java
@@ -2,10 +2,13 @@ package com.nubiform.payment.config;
 
 import com.nubiform.payment.security.AES256;
 import com.nubiform.payment.security.Encryption;
+import org.h2.tools.Server;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.NameTokenizers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
 
 @Configuration
 public class AppConfig {
@@ -22,5 +25,10 @@ public class AppConfig {
                 .setSourceNameTokenizer(NameTokenizers.UNDERSCORE)
                 .setDestinationNameTokenizer(NameTokenizers.UNDERSCORE);
         return modelMapper;
+    }
+
+    @Bean
+    public Server h2TcpServer() throws SQLException {
+        return Server.createTcpServer().start();
     }
 }
