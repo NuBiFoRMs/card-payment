31fb73d7c73de8d902ae097214f93ac29f652c36
NuBiFoRM <nubiform@me.com>
2021-05-09 19:26:32 +0900

    refactoring

--- src/main/java/com/nubiform/payment/validator/SubmitValidator.java
+++ src/main/java/com/nubiform/payment/validator/SubmitValidator.java
@@ -1,6 +1,5 @@
 package com.nubiform.payment.validator;
 
-import com.nubiform.payment.repository.HistoryRepository;
 import com.nubiform.payment.vo.SubmitRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,8 +13,6 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 @Component
 public class SubmitValidator implements Validator {
-
-    private final HistoryRepository historyRepository;
 
     @Override
     public boolean supports(Class<?> clazz) {
