db862d2629cfb78b5a7c326e3474da355fa512fd
NuBiFoRM <nubiform@me.com>
2021-06-04 16:18:43 +0900

    serializer

--- src/main/java/com/nubiform/payment/vo/payload/PayloadSerializer.java
+++ src/main/java/com/nubiform/payment/vo/payload/PayloadSerializer.java
@@ -4,17 +4,18 @@ import org.apache.commons.lang3.reflect.FieldUtils;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Objects;
 import java.util.Optional;
 
 public class PayloadSerializer {
 
     private static final int DEFAULT_PAYLOAD_SIZE = 500;
 
-    public static String serializer(Object object) {
+    public static String serializer(PayloadSerializable object) {
         StringBuilder payloadBuilder = new StringBuilder(DEFAULT_PAYLOAD_SIZE);
 
         Arrays.stream(FieldUtils.getAllFields(object.getClass()))
-                .filter(field -> Optional.ofNullable(field.getAnnotation(PayloadField.class)).isPresent())
+                .filter(field -> Objects.nonNull(field.getAnnotation(PayloadField.class)))
                 .sorted(Comparator.comparingInt(field -> field.getAnnotation(PayloadField.class).order()))
                 .forEach(field -> {
                     Optional.ofNullable(field.getAnnotation(PayloadField.class))
